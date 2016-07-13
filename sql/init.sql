26bab602f882df584a9e7ad79678ea9c713f2294
Carole Bou <35@p2-1.hbtn.io>
2016-07-13 14:08:32 -0700

    Correct script create users and databases

--- sql/init.sql
+++ sql/init.sql
@@ -1,27 +1,15 @@
 /* This script creates 2 different users with 2 different databases */
 
 -- Create users for SQL Authentication
-CREATE LOGIN airbnb_user_dev WITH PASSWORD = 'notSuitableForGitHub', DEFAULT_DATABASE = [airbnb_dev]
-GO
-CREATE LOGIN airbnb_user_prod WITH PASSWORD = 'notSuitableForGitHub', DEFAULT_DATABASE = [airbnb_prod]
-GO
+CREATE USER 'airbnb_user_prod'@'localhost' IDENTIFIED BY '';
+CREATE USER 'airbnb_user_dev'@'%' IDENTIFIED BY '';
 
 -- Create 2 databases in UTF8 (utf8_general_ci)
 CREATE DATABASE airbnb_dev CHARACTER SET utf8 COLLATE utf8_general_ci;
 CREATE DATABASE airbnb_prod CHARACTER SET utf8 COLLATE utf8_general_ci;
 
 -- Add user dev to database dev
-USE airbnb_dev;
-CREATE USER airbnb_user_dev FOR LOGIN airbnb_user_dev;
-EXEC sp_addrolemember 'db_datareader', 'airbnb_user_dev'
-EXEC sp_addrolemember 'db_datawriter', 'airbnb_user_dev'
-GO
+GRANT ALL PRIVILEGES ON database.airbnb_dev TO 'airbnb_user_dev'@'%' IDENTIFIED BY '';
 
 -- Add user prod to database prod
-USE airbnb_prod;
-CREATE USER airbnb_user_prod FOR LOGIN airbnb_user_prod;
-EXEC sp_addrolemember 'db_datareader', 'airbnb_user_prod'
-EXEC sp_addrolemember 'db_datawriter', 'airbnb_user_prod'
-GO
-
--- Create an environment variable AIRBNB_ENV with value development in computer and production in server
+GRANT ALL PRIVILEGES ON	database.airbnb_prod TO 'airbnb_user_prod'@'localhost' IDENTIFIED BY '';
